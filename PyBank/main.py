f40ef01f5f53d38e2d8aa4763d62c59a191d1f73
klinga <klinga88@gmail.com>
2018-05-23 21:36:51 -0400

    Updates to PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,14 @@
 #Andrew Kling
 #UNCC HW3
-#PyBank - Reads in multiple .csv files containing monthly revenue data from a bank
-#         Output will be pushed to new Revenue_Data_Summary.csv and terminal
-#         Output contains:
-#         -total count of months
-#         -total revenue
-#         -avg revenue change
-#         -greatest increase in revenue (month-year and value)
-#         -greatest decrease in revenue (month-year and value)
+#PyBank
+# Reads in multiple .csv files containing monthly revenue data from a bank
+# Output will be pushed to new Revenue_Data_Summary.csv and terminal
+# Output contains:
+# -total count of months
+# -total revenue
+# -avg revenue change
+# -greatest increase in revenue (month-year and value)
+# -greatest decrease in revenue (month-year and value)
 
 import os
 import pandas as pd
@@ -29,11 +30,7 @@ headers = ["Emp ID","First Name","Last Name", "DOB", "SSN", "State"]
 # Function Definitions
 #-----------------------------------------------------------
 
-#Converts Name field to two seperate fields, first name and last name
-def splitName(name):
-    first, last = name.split(" ")
-    print(f'First name: {first} | Last Name: {last}')
-    return (first, last)
+
 
 
 
@@ -41,34 +38,17 @@ def splitName(name):
 # Application Code
 #-----------------------------------------------------------
 
-#create csv file and add headers
-with open(outputFilePath,"w") as output:
-    writer = csv.writer(output)
-    writer.writerow(headers)
+##NOTES - use iloc with a for loop, using index to check values in current row and next row for month to
+# month changes
 
-#iterate over all the files stored in array, converting columns
-for f in files:
-    with open(f, newline="") as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=",")
-        for row in csvreader:
-            newRow=[]
-            #if employee ID is not a number, probably header so skip
-            if row[0].isdigit():
-                newRow.insert(0,row[0])
-                #split first and last name then insert last as new element
-                first, last = splitName(row[1])
-                newRow.insert(1, first)
-                newRow.insert(2,last)
-                #convert date to MM/DD/YYYY format
-                newRow.insert(3,convertDate(row[2]))
-                #Anonymize employee SSN
-                newRow.insert(4, anonymizeSSN(row[3]))
-                #convert state to abbreviation
-                newRow.insert(5, stateAbbrev[row[4]])
-                #append converted row to existing csv file
-                with open(outputFilePath,"a") as output:
-                    writer = csv.writer(output)
-                    writer.writerow(newRow)
-            
+#import budget csv files into DataFrame
+budgetData = pd.concat((pd.read_csv(f) for f in files), ignore_index=True)
 
+#count number of months in dataset
+numMonths = budgetData["Date"].count()
 
+#calculate the total revenue over the time period listed in the dataset
+totalRevenue = budgetData["Revenue"].sum()
+print(totalRevenue)
+
+budgetData.to_csv(outputFilePath)
